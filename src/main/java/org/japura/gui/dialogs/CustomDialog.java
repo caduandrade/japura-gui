df61d43a079ff793823570a5329e4fd0227ca4b8
Carlos Eduardo Leite de Andrade <cadu@japura.org>
2015-04-01 08:45:14 -0300

    adding esc key to dispose the dialogs

--- src/main/java/org/japura/gui/dialogs/CustomDialog.java
+++ src/main/java/org/japura/gui/dialogs/CustomDialog.java
@@ -228,6 +228,17 @@ public class CustomDialog {
   protected void buildDialog() {
     dialog = new JDialog();
 
+    ActionListener escListener = new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        dialog.dispose();
+      }
+    };
+
+    dialog.getRootPane().registerKeyboardAction(escListener,
+      KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+      JComponent.WHEN_IN_FOCUSED_WINDOW);
+
     dialog.setTitle(title);
 
     JPanel panel = new JPanel();
